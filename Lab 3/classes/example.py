bcb0abf4f0ae91f4a4ecf7e4243358e5b4bd32d1
tankisoff <156984891+tankisoff@users.noreply.github.com>
2024-02-08 00:56:37 +0600

    Add files via upload

---
+++ Lab 3/classes/example.py
@@ -0,0 +1,21 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
